2a19d69a56b2508d09fd7681939ad0b1e0757ea3
MARCORYKER <thangadanchajairohithyadav@gmail.com>
2025-07-02 03:08:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 💥 This tells Next to chill about linting during build
+  },
 };
 
 export default nextConfig;
